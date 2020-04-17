82e597fb295ade4b41d79df73413200d043b2be7
Ed Chapin <ed.chapin@nrc-cnrc.gc.ca>
2020-04-17 15:18:09 -0700

    Add script to run Monte Carlo coverage simulation

---
+++ coverage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#
+# This script implements a Monte Carlo distribution to test the
+# frequency of usable fields given modeled OIWFS probe geometries,
+# while avoiding the IRIS image/IFU pickoffs, and given a surface
+# density of potential guide stars.
+
+import numpy as np
+import oiwfs_sim
+import matplotlib.pyplot as plt
+
+# Establish the mean surface density of guide stars as a number of stars
+# per 2'-diameter NFIRAOS circular FOV
+num_per_FOV = 3.5
+FOV_area_arcminsq = np.pi*(1.)**2.
+density_perarcminsq = num_per_FOV/FOV_area_arcminsq
+
+# number of Monte Carlo simulations
+nmc = 100
+
+# Create OIWFS State object
+s = oiwfs_sim.State(None)
+
+# Outer loop over MC simulations
+results = []
+for i in range(nmc):
+    if i % 100 == 0:
+        print "%i / %i" % (i,nmc)
+    # Ensure that no probe is assigned to a star from previous iteration
+    for p in s.probes:
+        p.star = None
+
+    # Draw a Poisson-distributed samples of stars uniformly covering a 2'x2'
+    # square centered over the origin of the OIWFS coordinate system
+    l = (2.**2)*density_perarcminsq
+    n = np.random.poisson(l)
+    x_deg = np.random.uniform(low=-1.0/60.,high=1.0/60.,size=n)
+    y_deg = np.random.uniform(low=-1.0/60.,high=1.0/60.,size=n)
+
+    s.init_catalog(x_deg,y_deg)
+
+    # try assigning probes to stars
+    s.select_probes(catalog_subset=range(len(s.catalog_stars)))
+    #for p in s.probes:
+    #    if p.star is not None:
+    #        # Shouldn't be necessary but may catch an error
+    #        p.set_cart(p.star.x,p.star.y)
+    
+    nassigned = np.sum([p.park == False for p in s.probes])
+    #print n, nassigned
+    results.append([n,nassigned])
+
+results = np.array(results)
+
+print "Done:"
+for i in range(4):
+    print "%i stars %.1f %%" % (i,np.sum(results[:,1]==i))
+
